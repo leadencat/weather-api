1a7c65f230479da8f1c52e9b64c7cb6650710bf3
Jakob A <jdanders1999@gmail.com>
2024-01-06 22:53:34 -0500

    updated js, still has bugs. Code looks clear but then event listener on line 123 is not working, made a lot of changes trying to figure out a working event listener, updating the js with the new api keys, and debugging both js and html. Updated css so it looks okay

--- assets/script.js
+++ assets/script.js
@@ -1,89 +1,130 @@
 //basic elements
 var searchBtn = document.getElementById("searchBtn");
 var forecastsEl = document.getElementById("forecasts");
+var input = document.getElementById("input");
 
 var historyArray = loadHistory();
 
+//featch request for api data
 function convertSearch() {
-    var input = document.getElementById("input").value.trim();
+  var input = document.getElementById("input").value.trim();
 
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=1&appid=b539f961ee018c36b88d3838ba7bcfc2")
-        .then((response) => response.json())
-        .then(function (data) {
-            var lon = data[0].lon;
-            var lat = data[0].lat;
-            var city = data[0].name;
-            getWeatherData(lon, lat, city);
-            saveHistory(city);
-            updateHistory();
-            document.getElementById("input").value = "";
-        })
+  fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=1&appid=b539f961ee018c36b88d3838ba7bcfc2")
+  .then((response) => response.json())
+  .then(function (data) {
+      var lon = data[0].lon;
+      var lat = data[0].lat;
+      var city = data[0].name;
+      getWeatherData(lon, lat, city);
+      saveHistory(city);
+      updateHistory();
+      document.getElementById("input").value = "";
+  })
 }
 
+//featch request for api data
 function convertHistorySearch(historyBtnInfo) {
-    var input = historyBtnInfo.textContent;
+  var input = historyBtnInfo.textContent;
 
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=1&appid=b539f961ee018c36b88d3838ba7bcfc2")
-        .then((response) => response.json())
-        .then(function (data) {
-            var lon = data[0].lon;
-            var lat = data[0].lat;
-            var city = data[0].name;
-            getWeatherData(lon, lat, city);
-        })
+  fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=1&appid=b539f961ee018c36b88d3838ba7bcfc2")
+  .then((response) => response.json())
+  .then(function (data) {
+      var lon = data[0].lon;
+      var lat = data[0].lat;
+      var city = data[0].name;
+      getWeatherData(lon, lat, city);
+  })
 };
 
-//displaying api results
-function displayWeather(weatherData) {
-    weatherContainer.innerHTML = '';
-
-    const weatherCard = document.createElement('div');
-    weatherCard.className = 'card';
-
-    const weatherCity = document.createElement('h3');
-    weatherCity.textContent = weatherData.name;
-
-    const weatherTemp = document.createElement('p');
-    weatherTemp.textContent = `Temperature: ${weatherData.main.temp} °C`;
-
-    weatherCard.appendChild(weatherCity);
-    weatherCard.appendChild(weatherTemp);
-
-    weatherContainer.appendChild(weatherCard);
+function getWeatherData(lon, lat, city) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely&units=imperial&appid=3235f6ca43f152b21beee3053909231f")
+        .then((response) => response.json())
+        .then(function (data) {
+            currentDay(data, city);
+            fiveDay(data);
+        })
 }
 
-//local storage and event listener
-    searchButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    
-    var input = inputValue.value;
-    var inputValue =JSON.parse(localStorage.getItem("location"));
+//displaying api results
+function currentDay (data, city) {
+    var date = new Date((data.current.dt * 1000) - (data.timezone_offset * 1000))
+    var icon = data.current.weather[0].icon;
 
-    if (input === "") {
-        alert("Please select a location.")
+    var currentyCityEl = document.getElementById("currentCity");
+    var currentDateEl = document.getElementById("currentDate");
+    var currentIconEl = document.getElementById("currentIcon");
+    var currentTempEl = document.getElementById("currentTemp");
+    var currentHumidityEl = document.getElementById("currentHumidity");
+    var currentWindEl = document.getElementById("currentWind");
 
-        localStorage.setItem("submitButton", "date");
-        renderLastRegistered();
+    currentyCityEl.textContent = city;
+    currentDateEl.textContent = date.toLocaleDateString("en-US");
+    currentIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + icon + "@2x.png");
+    currentTempEl.textContent = data.current.temp;
+    currentHumidityEl.textContent = data.current.humidity;
+    currentWindEl.textContent = data.current.wind_speed;
+}
+
+function fiveDay(data) {
+    for (var i = 1; i < 6; i++) {
+        var date = new Date((data.daily[i].dt * 1000) - (data.timezone_offset * 1000));
+        var icon = data.daily[i].weather[0].icon;
+
+        var dateEl = document.getElementById("date" + i);
+        var iconEl = document.getElementById("icon" + i);
+        var tempEl = document.getElementById("temp" + i);
+        var humidityEl = document.getElementById("humidity" + i);
+        var windEl = document.getElementById("wind" + i);
+
+        dateEl.textContent = date.toLocaleDateString("en-US");
+        iconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + icon + "@2x.png");
+        tempEl.textContent = "Temp: " + data.daily[i].temp.day + "°F";
+        humidityEl.textContent = "Humidity: " + data.daily[i].humidity + "%";
+        windEl.textContent = "Wind Spd: " + data.daily[i].wind_speed + "mph";
+
+        forecastsEl.classList.remove("hidden");
+}
+}
+
+//local storage
+function saveHistory(city) {
+  if (!historyArray) {
+    historyArray = [city];
+  } else {
+    historyArray.unshift(city);
+  }
+
+  if (historyArray.length > 5) {
+    var delExtra = historyArray.pop();
+  }
+
+  localStorage.setItem("history", JSON.stringify(historyArray));
+}
+
+function loadHistory() {
+  var historyArray = JSON.parse(localStorage.getItem("history"));
+
+  return historyArray;
+}
+
+function updateHistory() {
+    if (historyArray != null) {
+        for (var i = 0; i < historyArray.length; i++) {
+            var historyBtn = document.getElementById("btn" + i);
+            historyBtn.textContent = historyArray[i];
+            historyBtn.addEventListener("click", function() {convertHistorySearch(event.target)});
+        }
+    }
+}
+
+//event listener for seach button
+searchBtn.addEventListener("click", convertSearch);
+//not sure why my input listener is not defined
+input.addEventListener("click", function (event) {
+    if (event.key === "Enter") {
+        document.getElementById("searchBtn").click();
     }
 });
 
-//local storage for weather api
-localStorage.setItem("inputLocation", city);
-localStorage.setItem("inputZipCode", zipCode);
-
-fetchWeatherData(city);
-
-function renderLastRegistered() {
-const lastCity = localStorage.getItem("inputLocation");
-const lastZipCode = localStorage.getItem("inputZipCode");
-
-if (lastCity) {
-    inputLocation.value = lastCity;
-}
-
-if (lastZipCode) {
-    inputZipCode.value = lastZipCode;
-}
-}
-
-renderLastRegistered();
+loadHistory();
+updateHistory();
